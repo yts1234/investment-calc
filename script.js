af68e2db5e8d486e34d0fcb4f3abd3f9df4f079d
Yosua Tito <yosua.sumbogo@amartha.com>
2025-04-04 15:30:46 +0700

    - Change currency notation - Pretify the input using toLocaleString - Fix investment calculation logic - support tenor 4y

--- script.js
+++ script.js
@@ -4,16 +4,18 @@ const bondProfiles = [
     name: "ST014-T2 tenor 2 tahun",
     interestRate: 6.5, // Annual interest (%)
     tenure: 24, // Months
-    minInvestment: 1000000, // Minimum USD
+    minInvestment: 1000000, // Minimum IDR
     tax: 0.1, // tax
+    unit: 0
   },
   {
     id: 2,
     name: "ST014-T4 tenor 4 tahun",
-    interestRate: 6.0,
+    interestRate: 6.6,
     tenure: 48,
     minInvestment: 1000000,
     tax: 0.1,
+    unit: 0
   },
 ];
 
@@ -36,7 +38,7 @@ bondDropdown.addEventListener("change", () => {
   selectedBond = bondProfiles.find((bond) => bond.id === bondId);
   
   // Show minimum investment
-  document.getElementById("minValue").textContent = selectedBond.minInvestment;
+  document.getElementById("minValue").textContent = formatNumber(selectedBond.minInvestment);
 });
 
 // Real-time validation
@@ -62,6 +64,7 @@ document.getElementById("investmentForm").addEventListener("submit", (e) => {
   }
 
   const amount = parseFloat(document.getElementById("investmentAmount").value);
+  unit = amount / selectedBond.minInvestment
   
   // Final validation
   if (amount < selectedBond.minInvestment) {
@@ -70,22 +73,33 @@ document.getElementById("investmentForm").addEventListener("submit", (e) => {
   }
 
   // Calculate long coupun
-  const longCouponA = Math.floor((amount * 1/12 * 17/30)) * (selectedBond.interestRate / 100)
-  const longCouponB = Math.floor((amount * 1/12)) * (selectedBond.interestRate / 100)
-  const initLongCoupon = Math.floor((longCouponA + longCouponB)) * govBondTax
+  const longCouponA = Math.round((selectedBond.minInvestment * 1/12 * 17/30) * (selectedBond.interestRate / 100))
+  const longCouponB = Math.round((selectedBond.minInvestment * 1/12) * (selectedBond.interestRate / 100))
+  // const totalLongInitCoupon = (longCouponA + longCouponB) * unit
+  const initLongCouponNett = Math.round((longCouponA + longCouponB) * unit * govBondTax)
 
   // Calculate total return
-  const annualInterest = Math.floor((amount * 1/12 * (selectedBond.interestRate / 100)) * (selectedBond.tenure-2)) * govBondTax;
-  total = amount + initLongCoupon + annualInterest;
+  const monthlyInterest = Math.round((selectedBond.minInvestment * 1/12 * (selectedBond.interestRate / 100))); 
+  const monthlyInterestNett = (monthlyInterest * unit) - (monthlyInterest * unit * 0.1)
+  const annualInterest = Math.round(monthlyInterestNett * (selectedBond.tenure-2));
+  total = amount + initLongCouponNett + annualInterest;
 
   // Display result
   // console.log("longCoupunA: ", longCouponA)
-  // console.log("longCoupunb: ", longCouponB)
-  // console.log("initLongCoupun: ", initLongCoupon)
+  // console.log("longCoupunB: ", longCouponB)
+  // console.log("initial longCoupunNett: ", initLongCouponNett)
+  // console.log("Monthly Interest: ", monthlyInterest)
+  // console.log("Monthly InterestAfterTax: ", monthlyInterestAfterTax)
+  // console.log("Monthly InterestNett: ", monthlyInterestNett)
   // console.log("Annual Interest: ", annualInterest)
   // console.log("Total: ", total)
 
   document.getElementById("finalAmount").textContent = 
-    `$${amount.toFixed(2)} → $${total.toFixed(2)} in ${selectedBond.tenure} months`;
+    `Rp`+formatNumber(amount)+` → Rp`+formatNumber(total)+` in ${selectedBond.tenure} months`;
   document.getElementById("result").style.display = "block";
 });
+
+function formatNumber(number){
+  return number.toLocaleString('id-ID');
+}
+console.log(Math.round(54166.667))
